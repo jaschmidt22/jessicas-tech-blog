f393477d3ae9c2b4be876e8d5903f5ab50651b1b
jaschmidt22 <146290812+jaschmidt22@users.noreply.github.com>
2024-02-04 14:22:03 -0700

    added seeds

--- seeds/postData.js
+++ seeds/postData.js
@@ -1,7 +1,33 @@
+const { Post } = require("../models");
+//array of sample data to seed post table
 const postData = [
   {
     title: "First Blog Post",
     content: "I love this product.",
     user_id: 1,
   },
-]; //continue to add more posts
+  {
+    title: "Second Blog Post",
+    content: "This product is great.",
+    user_id: 2,
+  },
+  {
+    title: "Third Blog Post",
+    content: "This product is okay.",
+    user_id: 3,
+  },
+  {
+    title: "Fourth Blog Post",
+    content: "I don't like this product.",
+    user_id: 4,
+  },
+  {
+    title: "Fifth Blog Post",
+    content: "I hate this product.",
+    user_id: 5,
+  },
+];
+//function to seed posts table with sample data
+const seedPosts = () => Post.bulkCreate(postData);
+
+module.exports = seedPosts;
