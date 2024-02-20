ad196424f58aead8bfafc5b1a725939194fa045d
jaschmidt22 <146290812+jaschmidt22@users.noreply.github.com>
2024-02-19 18:00:21 -0700

    jawsdburl

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,6 @@
 const Sequelize = require("sequelize");
 require("dotenv").config(); //import password from .env
 
-// const sequelize = process.env.JAWSDB_URL
-//   ? new Sequelize(process.env.JAWSDB_URL)
-//   : new Sequelize(
-//       process.env.DB_NAME,
-//       process.env.DB_USER,
-//       process.env.DB_PASSWORD,
-//       {
-//         host: "localhost",
-//         dialect: "mysql",
-//         dialectOptions: {
-//           decimalNumbers: true,
-//         },
-//       }
-//     );
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
